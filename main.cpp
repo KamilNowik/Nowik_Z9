9ded13454d760318fa108e54145ee835a291cb9d
KamilNowik <34374860+KamilNowik@users.noreply.github.com>
2018-01-23 16:14:08 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct rekord
+  {
+      char znak;
+      int liczba;
+  };
+
+
+
+void wypelnianie(T[][n], char min, char max, int min, int max])
+{
+    for (int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            T[i][j].znak=
+        }
+    }
+
+
+
+
+
+}
+
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
